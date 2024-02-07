083998bf14cedcae93f4fda134256165d3ed7ab9
Alan <62551143+alan-vle@users.noreply.github.com>
2024-02-07 14:08:46 +0100

    Feat #2: install tailwind (#16)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const tailwind = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = tailwind
