29c9dd3b6170c3c398d37dd2498a4001f9469b30
AlanV. <scoutilepudou@gmail.com>
2024-02-07 14:12:36 +0100

    Feat #3: install a theme for tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import {Button} from "@material-tailwind/react";
 
 function App() {
 
   return (
-    <>
+    <><Button>Button</Button>
         <h1 className="text-3xl font-bold underline">
             Hello world!
         </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-const tailwind = {
+/** @type {(tailwindConfig: object) => object} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+const tailwind = withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -8,6 +10,6 @@ const tailwind = {
     extend: {},
   },
   plugins: [],
-}
+})
 
 module.exports = tailwind
