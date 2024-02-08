3ee6465df40468b79ef82ec8026aef2d20983073
Alan <62551143+alan-vle@users.noreply.github.com>
2024-02-08 10:16:40 +0100

    Feat #5: create navbar footer components (#19)

---
+++ src/components/parts/Navbar.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import {
+    Navbar,
+    Collapse,
+    Typography,
+    IconButton,
+} from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleUser} from "@fortawesome/free-regular-svg-icons";
+import luxlifeLogo from "../../assets/luxlife_logo.png";
+function NavList() {
+    return (
+        <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+                <Typography
+                    as="a"
+                    variant="h6"
+                    className="cursor-pointer py-1.5"
+                >
+                    <Link to="/agencies" className="flex items-center hover:text-blue-500 transition-colors">
+                        Nos agences
+                    </Link>
+                </Typography>
+                <div className={"py-1.5 font-bold"}>|</div>
+                <Typography
+                    as="a"
+                    variant="h6"
+                    className=" cursor-pointer py-1.5"
+                >
+                    <Link to="/about-us" className="flex items-center hover:text-blue-500 transition-colors">A propos</Link>
+                </Typography>
+        </ul>
+    );
+}
+
+function NavbarSimple() {
+    const [openNav, setOpenNav] = React.useState(false);
+
+    const handleWindowResize = () =>
+        window.innerWidth >= 960 && setOpenNav(false);
+
+    React.useEffect(() => {
+        window.addEventListener("resize", handleWindowResize);
+
+        return () => {
+            window.removeEventListener("resize", handleWindowResize);
+        };
+    }, []);
+
+    return (
+        <Navbar className="mx-auto px-6 py-3" fullWidth={true}>
+            <div className="flex items-center justify-between text-blue-gray-900">
+                <div className="hidden lg:block">
+                    <NavList />
+                </div>
+                <div className="hidden gap-1 lg:flex items-center justify-center">
+                    <img src={luxlifeLogo} alt="Luxlife logo" className="w-[200px] h-[150px] overflow-hidden absolute"/>
+                </div>
+                <div className="hidden gap-2 lg:flex">
+                    <FontAwesomeIcon icon={faCircleUser} className={"py-2"} size={"lg"}/>
+                    <Typography
+                        as="a"
+                        variant="h6"
+                        className="cursor-pointer py-1.5"
+                    >
+                        <Link to={"/login"} className="flex items-center hover:text-blue-500 transition-colors">
+                            Connexion
+                        </Link>
+                    </Typography>
+                    <div className={"py-1.5 font-bold"}>|</div>
+                    <Typography
+                        as="a"
+                        variant="h6"
+                        className="cursor-pointer py-1.5"
+                    >
+                        <Link to={"/register"} className="flex items-center hover:text-blue-500 transition-colors">
+                            Inscription
+                        </Link>
+                    </Typography>
+                </div>
+                <IconButton
+                    variant="text"
+                    className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                    ripple={false}
+                    onClick={() => setOpenNav(!openNav)}
+                >
+                    {openNav ? (
+                        <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+                    ) : (
+                        <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+                    )}
+                </IconButton>
+            </div>
+            <Collapse open={openNav}>
+                <NavList />
+            </Collapse>
+        </Navbar>
+    );
+}
+
+export default NavbarSimple;
