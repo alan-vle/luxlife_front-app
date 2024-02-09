dc4cd5197594f83056fcb9036cccc11053d8ca0c
AlanV. <scoutilepudou@gmail.com>
2024-02-09 17:41:06 +0100

    Feat #20: use docker

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,14 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    host: true,
+    port: 8000, // This is the port which we will use in docker
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true
+    }
   }
 })
